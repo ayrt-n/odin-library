7e1ff5395463c0d8b6877223e07510e266ea324b
ayrton <ayrton.parkinson1@gmail.com>
2022-08-06 12:26:58 -0400

    Add displayLibrary and helper function addBookToTable

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 let myLibrary = [];
+let libraryTable = document.getElementById("library")
 
 // Book constructor function 
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
-  this.read = Boolean(read)
+  this.read = read ? "Yes" : "No"
 }
 
 // Add a book to myLibrary
@@ -13,3 +14,23 @@ function addBookToLibrary(title, author, pages, read) {
   let book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
+// Fill table with some seed data for testing
+addBookToLibrary("Catcher and the Raisin", "Bobby Dumpins", 230, 0)
+addBookToLibrary("Clockwork Doorhinge", "Amy Pancakes", 700, 1)
+
+// Display library in library table
+function displayLibrary() {
+  myLibrary.forEach(addBookToTable)
+}
+
+// Build row in library table
+function addBookToTable(book) {
+  let row = libraryTable.insertRow();
+  row.insertCell(0).innerHTML = book.title
+  row.insertCell(1).innerHTML = book.author
+  row.insertCell(2).innerHTML = book.pages
+  row.insertCell(3).innerHTML = book.read
+}
+
+displayLibrary();
