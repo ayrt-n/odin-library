58a10fffe06178974356c5fabc3d535569af16ee
ayrton <ayrton.parkinson1@gmail.com>
2022-08-07 10:05:48 -0400

    Add ability to submit new book and display in library table

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
+// Get Elements
 let myLibrary = [];
-let libraryTable = document.getElementById("library")
+let libraryTable = document.getElementById("library");
+let newBookButton = document.getElementById("new-book-button");
+let newBookForm = document.getElementById("book-form");
+let addBookButton = document.getElementById("add-book-button")
+
+// Event Listeners
+newBookButton.addEventListener("click", toggleBookForm);
+addBookButton.addEventListener("click", submitNewBook);
 
 // Book constructor function 
 function Book(title, author, pages, read) {
@@ -24,6 +32,21 @@ function displayLibrary() {
   myLibrary.forEach(addBookToTable)
 }
 
+// Add new book to table and library
+function submitNewBook() {
+  const form_elements = newBookForm.elements;
+
+  let title = form_elements["title"].value;
+  let author = form_elements["author"].value;
+  let pages = form_elements["pages"].value;
+  let read = form_elements["read"].value;
+
+  addBookToLibrary(title, author, pages, read);
+  addBookToTable(myLibrary[myLibrary.length - 1]);
+
+  newBookForm.reset()
+}
+
 // Build row in library table
 function addBookToTable(book) {
   let row = libraryTable.insertRow();
@@ -31,6 +54,14 @@ function addBookToTable(book) {
   row.insertCell(1).innerHTML = book.author
   row.insertCell(2).innerHTML = book.pages
   row.insertCell(3).innerHTML = book.read
+  row.insertCell(4).innerHTML = ''
+  row.insertCell(5).innerHTML = "Toggle Read"
+  row.insertCell(6).innerHTML = "Delete"
+}
+
+// Toggle display of new book form
+function toggleBookForm() {
+  newBookForm.classList.toggle("is-active")
 }
 
 displayLibrary();
